b129bcf3d7221583a560d8da6ec4bdc384c81e93
csuser <csuser@160.10.217.82>
2018-10-25 16:25:01 -0400

    Added checkName method

--- Time Management/src/edu/westga/cs3211/time_management/model/EventDataValidator.java
+++ Time Management/src/edu/westga/cs3211/time_management/model/EventDataValidator.java
@@ -2,8 +2,30 @@ package edu.westga.cs3211.time_management.model;
 
 /** Validate Event information.
  * 
- * @author jcorley
+ * @author Tristen Rivera, Tyler Scott, Dexter Tarver
  */
 public class EventDataValidator {
+	
+	/**Checks if the even name is valid
+	 * 
+	 * @precondition name must be < 60 characters and name cannot be empty
+	 * @postcondition none
+	 * 
+	 * @param name the name of the event
+	 * @return true if the name is valid
+	 */
+	public boolean checkName(String name) {
+		
+		boolean result;
+		
+		if(name.length() > 60) {
+			result = false;
+		}else if(name.isEmpty()) {
+			result = false;
+		}else {
+			result = true;
+		}
+		return result;
+	}
 
 }
