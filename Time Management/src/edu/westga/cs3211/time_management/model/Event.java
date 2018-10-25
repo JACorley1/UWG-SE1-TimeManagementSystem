3cd4a9be0523f3fa5a620bae7b3a46bd6ca21e41
JACorley1 <jcorley@westga.edu>
2018-10-25 16:00:57 -0400

    Adding basic structure for system model.

---
+++ Time Management/src/edu/westga/cs3211/time_management/model/Event.java
@@ -0,0 +1,9 @@
+package edu.westga.cs3211.time_management.model;
+
+/** Store basic information for an event.
+ * 
+ * @author csuser
+ */
+public class Event {
+
+}
---
+++ Time Management/src/edu/westga/cs3211/time_management/model/EventDataValidator.java
@@ -0,0 +1,9 @@
+package edu.westga.cs3211.time_management.model;
+
+/** Validate Event information.
+ * 
+ * @author csuser
+ */
+public class EventDataValidator {
+
+}
