a91cac845afa3400355be103c0b28e4318ce9175
csuser <csuser@160.10.217.65>
2018-10-25 16:08:54 -0400

    Added empty checkStartTime() method and @authors.

--- Time Management/src/edu/westga/cs3211/time_management/model/EventDataValidator.java
+++ Time Management/src/edu/westga/cs3211/time_management/model/EventDataValidator.java
@@ -1,9 +1,18 @@
 package edu.westga.cs3211.time_management.model;
 
+import java.time.LocalTime;
+
 /** Validate Event information.
  * 
- * @author csuser
+ * @author Dylan Knox, Kyle Riggs, Cody Graham
  */
 public class EventDataValidator {
 
+	/**
+	 * Checks start time.
+	 * 
+	 */
+	public void checkStartTime(LocalTime startTime) {
+		
+	}
 }
