8872f6f955e7d1f227182acd8b978283fe6df93f
us <vjenkin2@my.westga.edu>
2018-10-25 16:09:05 -0400

    commit

--- Time Management/src/edu/westga/cs3211/time_management/model/Calendar.java
+++ Time Management/src/edu/westga/cs3211/time_management/model/Calendar.java
@@ -1,9 +1,20 @@
 package edu.westga.cs3211.time_management.model;
 
+import java.util.ArrayList;
+
 /** Store and manage a collection of events.
  * 
- * @author csuser
+ * @author Victoria Jenkins, Laura Smedley, Jonathan Nicholl, Brandon Walker
  */
 public class Calendar {
 
+
+	private ArrayList<Event> events;
+	
+	public Calendar() {
+		this.events = new ArrayList<Event>();
+	}
+	public void declareConflict() {
+		
+	}
 }
