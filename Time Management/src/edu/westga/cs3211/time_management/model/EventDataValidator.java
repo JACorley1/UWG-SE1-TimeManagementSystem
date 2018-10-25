830b014de56f0de8bbd2ee849d86f2a9b25598ff
The_Jeselnik <djeseln1@my.westga.edu>
2018-10-25 16:25:34 -0400

    commit before pull

--- Time Management/src/edu/westga/cs3211/time_management/model/EventDataValidator.java
+++ Time Management/src/edu/westga/cs3211/time_management/model/EventDataValidator.java
@@ -2,18 +2,21 @@ package edu.westga.cs3211.time_management.model;
 
 import java.util.List;
 
-/** Validate Event information.
+/**
+ * Validate Event information.
  * 
  * @author Daniel Jeselnik, Dylan McCleskey, Justin Smith
  */
 public class EventDataValidator {
-	
+
 	public void checkAttendees(List<String> names) {
+		if (names == null) {
+			
+		}
 		List<String> attendees = names;
-		for (var curAttendee : attendees) {
+		for (String curAttendee : attendees) {
 			
 		}
 	}
-	
-	
+
 }
