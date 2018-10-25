8800092d0ff67243744e3cebcd3b752504a64fbe
The_Jeselnik <dannyjeselnik@gmail.com>
2018-10-25 16:09:31 -0400

    stubbed chleckAttendees method

--- Time Management/src/edu/westga/cs3211/time_management/model/EventDataValidator.java
+++ Time Management/src/edu/westga/cs3211/time_management/model/EventDataValidator.java
@@ -1,9 +1,19 @@
 package edu.westga.cs3211.time_management.model;
 
+import java.util.List;
+
 /** Validate Event information.
  * 
- * @author csuser
+ * @author Daniel Jeselnik, Dylan McCleskey, Justin Smith
  */
 public class EventDataValidator {
-
+	
+	public void checkAttendees(List<String> names) {
+		List<String> attendees = names;
+		for (var curAttendee : attendees) {
+			
+		}
+	}
+	
+	
 }
